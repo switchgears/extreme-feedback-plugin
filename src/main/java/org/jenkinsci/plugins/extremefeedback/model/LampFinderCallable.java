71c12ef1a8e961dd49775f8e6453320c79b8b1c6
Aske Olsson <aske.olsson@switch-gears.dk>
2013-05-07 16:06:21 +0200

    Enables lamp discovery from web ui
    Change-Id: I26ba57bf7e26d04bdfef5c74b87164684c8b7f52
    Signed-off-by: Aske Olsson <aske.olsson@switch-gears.dk>

--- src/main/java/org/jenkinsci/plugins/extremefeedback/model/LampFinderCallable.java
+++ src/main/java/org/jenkinsci/plugins/extremefeedback/model/LampFinderCallable.java
@@ -1,30 +1,34 @@
 package org.jenkinsci.plugins.extremefeedback.model;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang.StringUtils;
 
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.SocketTimeoutException;
-import java.util.Arrays;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class LampFinderCallable implements Callable<Set<Lamp>> {
 
     Set<Lamp> lamps = Sets.newTreeSet();
     private static final int PORT = 19418;
     private static final byte[] MESSAGE = "EXTREMEFEEDBACK.COM".getBytes();
+    private static final Logger LOGGER = Logger.getLogger("jenkins.plugins.extremefeedback");
 
     public Set<Lamp> call() throws Exception {
         // Send
         MulticastSocket mcs = new MulticastSocket(PORT+1);
-        InetAddress inetAddress = InetAddress.getByName("255.255.255.255");
+        InetAddress inetAddress = InetAddress.getByName("239.77.124.213");
         mcs.send(new DatagramPacket(MESSAGE, MESSAGE.length, inetAddress, PORT));
-
         // Receive
+        mcs.joinGroup(inetAddress);
         mcs.setSoTimeout(5000);
         boolean exit = false;
         while(!exit) {
@@ -32,10 +36,13 @@ public class LampFinderCallable implements Callable<Set<Lamp>> {
             DatagramPacket receiver = new DatagramPacket(buf, buf.length);
             try {
                 mcs.receive(receiver);
-                String data = Arrays.toString(receiver.getData());
+                String data = new String(receiver.getData());
+                LOGGER.log(Level.INFO, data);
                 if(data.startsWith("MAC=")) {
                     String macAddress = Iterables.getLast(Splitter.on("=").trimResults().split(data));
+                    macAddress = StringUtils.chomp(StringUtils.trim(macAddress));
                     String ipAddress = receiver.getAddress().getHostAddress();
+                    LOGGER.log(Level.INFO, "MAC Address: " + macAddress + " IP  Address: " + ipAddress);
                     Lamp lamp = new Lamp(macAddress, ipAddress);
                     lamps.add(lamp);
                 }
@@ -43,7 +50,7 @@ public class LampFinderCallable implements Callable<Set<Lamp>> {
                 exit = true;
             }
         }
-
+        mcs.leaveGroup(inetAddress);
         return lamps;
     }
 
