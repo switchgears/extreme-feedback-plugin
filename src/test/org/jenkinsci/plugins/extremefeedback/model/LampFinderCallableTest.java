06a0a4efb42a251823eee07f37870521429388e6
Emanuele Zattin <emanuele.zattin@switch-gears.dk>
2013-06-19 22:06:58 +0200

    Add unit test for mac extraction

--- src/test/org/jenkinsci/plugins/extremefeedback/model/LampFinderCallableTest.java
+++ src/test/org/jenkinsci/plugins/extremefeedback/model/LampFinderCallableTest.java
@@ -13,6 +13,7 @@ public class LampFinderCallableTest {
         Map<String, String> macs =ImmutableMap.of(
                 "MAC=11:11:11:11:11:11", "11:11:11:11:11:11",
                 "MAC=aa:aa:aa:aa:aa:aa", "aa:aa:aa:aa:aa:aa",
+                "MAC=b8:27:eb:16:c9:a8", "b8:27:eb:16:c9:a8",
                 "MAC=11:11:11:11:11:11    ,;+   \n", "11:11:11:11:11:11",
                 "MAC=11:11:11:11:11:11    ,=;+   \n", "11:11:11:11:11:11"
         );
